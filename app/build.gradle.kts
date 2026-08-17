06a3282b27e0412fd87367aca2c5080d7d22c629
James Lee <noob76379@gmail.com>
2026-08-17 16:54:15 +0530

    Bump app version to 4.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "xyz.mpv.rex"
     minSdk = 26
     targetSdk = 36
-    versionCode = 212
-    versionName = "4.5.2"
+    versionCode = 213
+    versionName = "4.5.3"
 
     vectorDrawables {
       useSupportLibrary = true
